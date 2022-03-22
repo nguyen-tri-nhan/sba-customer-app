c4c4197435a020ecd224784778a1f554e21ef8ab
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-03-22 21:44:39 +0700

    ios config

--- model/MAuth.js
+++ model/MAuth.js
@@ -1,32 +1,32 @@
-import PageRouter from '../routes/PageRouter';
-import Services from '../util/Services';
+// import PageRouter from '../routes/PageRouter';
+// import Services from '../util/Services';
 
-const MAuth = {
-  login(user) {
-    Services.login(user).then(({ data }) => {
-      localStorage.setItem("JWT", `Bearer ${data.accessToken}`);
-    })
-      .then(() => {
-        Services.getMe().then(({ data }) => {
-          localStorage.setItem("User", JSON.stringify(data));
-        })
-          .then(() => {
-            PageRouter.redirect("/");
-          })
-      })
+// const MAuth = {
+//   login(user) {
+//     Services.login(user).then(({ data }) => {
+//       localStorage.setItem("JWT", `Bearer ${data.accessToken}`);
+//     })
+//       .then(() => {
+//         Services.getMe().then(({ data }) => {
+//           localStorage.setItem("User", JSON.stringify(data));
+//         })
+//           .then(() => {
+//             PageRouter.redirect("/");
+//           })
+//       })
 
-  },
-  logout() {
-    localStorage.clear();
-  },
-  isLoggedIn() {
-    return localStorage.getItem("JWT") ? true : false;
-  },
-  getMe() {
-    Services.getMe().then(({ data }) => {
-      localStorage.setItem("User", JSON.stringify(data));
-    })
-  },
-}
+//   },
+//   logout() {
+//     localStorage.clear();
+//   },
+//   isLoggedIn() {
+//     return localStorage.getItem("JWT") ? true : false;
+//   },
+//   getMe() {
+//     Services.getMe().then(({ data }) => {
+//       localStorage.setItem("User", JSON.stringify(data));
+//     })
+//   },
+// }
 
-export default MAuth;
+// export default MAuth;

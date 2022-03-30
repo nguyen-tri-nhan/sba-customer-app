488fafccba4b10174f4fb17efad950f7f74a9395
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-03-30 09:35:12 +0700

    add date picker

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 import { registerRootComponent } from 'expo';
+import { Platform } from 'react-native'
 
 import App from './App';
 
+const isAndroid = Platform.OS === 'android';
+const isHermesEnabled = !!global.HermesInternal;
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+console.log('isHermesEnabled', isHermesEnabled)
+console.log('isAndroid', isAndroid)
+
+if (isHermesEnabled || isAndroid) {  // this line is only needed if you don't use an .android.js file
+
+
+
+  require('@formatjs/intl-getcanonicallocales/polyfill');
+  require('@formatjs/intl-locale/polyfill');
+
+
+  require('@formatjs/intl-pluralrules/polyfill');
+  require('@formatjs/intl-pluralrules/locale-data/en.js'); // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+
+  require('@formatjs/intl-displaynames/polyfill');
+  require('@formatjs/intl-displaynames/locale-data/en.js'); // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+
+  require('@formatjs/intl-listformat/polyfill');
+  require('@formatjs/intl-listformat/locale-data/en.js'); // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+
+  require('@formatjs/intl-numberformat/polyfill');
+  require('@formatjs/intl-numberformat/locale-data/en.js'); // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+
+  require('@formatjs/intl-relativetimeformat/polyfill');
+  require('@formatjs/intl-relativetimeformat/locale-data/en.js'); // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+
+  require('@formatjs/intl-datetimeformat/polyfill');
+  require('@formatjs/intl-datetimeformat/locale-data/en.js'); // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+
+  require('@formatjs/intl-datetimeformat/add-golden-tz.js');
+
+
+
+  // https://formatjs.io/docs/polyfills/intl-datetimeformat/#default-timezone
+
+  if ('__setDefaultTimeZone' in Intl.DateTimeFormat) {
+
+    // If you are using react-native-cli
+    let RNLocalize = require('react-native-localize');
+    Intl.DateTimeFormat.__setDefaultTimeZone(RNLocalize.getTimeZone());
+
+  }
+} 
+
 registerRootComponent(App);

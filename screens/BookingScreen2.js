1386459c841dcbe000fb49837cb8e71e0fa26721
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-04-01 22:14:46 +0700

    forwarded to disvoice

--- screens/BookingScreen2.js
+++ screens/BookingScreen2.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { SafeAreaView, TouchableOpacity } from 'react-native';
-import { useEffect, useState } from 'react';
+import { SafeAreaView, TouchableOpacity, ScrollView } from 'react-native';
 import BookingStepIndicator from '../components/BookingStepIndicator';
 import { Text } from '../components/Themed';
-import Services from '../utils/Services';
 import { Button, Card } from 'react-native-paper';
 import { useStyle } from '../utils/style';
 import { toVND } from '../utils/CurrencyHelper';
@@ -15,19 +13,40 @@ function BookingScreen2(props) {
 
   const styles = useStyle();
   const onContinuePress = () => {
-    navigation.push("Booking3", { pkg });
+    navigation.push("Booking3", { pkg, forwardedItems, totalPrice, showroom });
   }
+
+  console.log(forwardedItems);
   return (
     <SafeAreaView style={styles.packageDetailsContainer}>
       <BookingStepIndicator currentStep={1} />
       <Card style={styles.customerInformation}>
-        <Text>
-          {pkg.name}
-        </Text>
+        <ScrollView>
+          <Text>
+            Gói dịch vụ: {pkg.name}
+          </Text>
+          <Text>
+            Địa điểm: {pkg.location}
+          </Text>
+          <Text>
+            Chi nhánh thực hiện: {showroom.name}
+          </Text>
+          <Text>
+            Địa chỉ: {showroom.address}
+          </Text>
+          <Text>
+            Dịch vụ thêm:
+          </Text>
+          {
+            forwardedItems.map((item) => (<>
+              <Text>{item.itemName} {item.amount}</Text>
+            </>))
+          }
+        </ScrollView>
       </Card>
       <Card style={styles.packageDetailsFooter}>
         <TouchableOpacity onPress={onContinuePress} style={styles.packageDetailsBookingButton}>
-          <Button>Đặt ngay: {toVND(totalPrice)}</Button>
+          <Button>Thanh toán: {toVND(totalPrice)}</Button>
         </TouchableOpacity>
       </Card>
     </SafeAreaView>

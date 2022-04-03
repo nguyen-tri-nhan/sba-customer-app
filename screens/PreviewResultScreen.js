d49ddb82e232888d3af8956cba210d42f0ecf050
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-04-03 20:38:36 +0700

    screen

--- screens/PreviewResultScreen.js
+++ screens/PreviewResultScreen.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 
 function PreviewResultScreen(props) {
+  const { navigation, route } = props;
+  const { params } = route;
+  const { links } = params;
+
+  const renderLinks = () => {
+    return links.map((link) => {
+      <Image source={link} />
+    });
+  }
+
   return (
     <View>
-      
+      {links ?
+        renderLinks() :
+        (<Image source={require("../assets/style_14.jpg")} />)}
     </View>
   );
 }

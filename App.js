73ddc4b9ac5c780423acf12f35c5484bc077c535
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-04-05 22:54:50 +0700

    ignore warning of pkg detail

--- App.js
+++ App.js
@@ -5,11 +5,12 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { useLoadedAssets } from "./hooks/useLoadedAssets";
 import Navigation from "./navigation";
-import { useColorScheme } from "react-native";
+import { useColorScheme, LogBox } from "react-native";
 
 export default function App() {
   const isLoadingComplete = useLoadedAssets();
   const colorScheme = useColorScheme();
+  LogBox.ignoreLogs(['Warning: Cannot update a component (`PackageDetailsScreen`)']);
 
   if (!isLoadingComplete) {
     return null;

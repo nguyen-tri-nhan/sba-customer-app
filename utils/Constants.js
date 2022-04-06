5cd7d9d101ce23e27b92e0eaa875a57eb9536c2b
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-04-06 10:57:47 +0700

    add local api

--- utils/Constants.js
+++ utils/Constants.js
@@ -1,6 +1,7 @@
 // const domain = 'http://sbaprj-dev.eba-dyjmryhk.ap-southeast-2.elasticbeanstalk.com';
 // const domain = 'http://localhost:5000';
 // const domain = 'http://192.168.3.100:5000';
+// const domain = 'http://192.168.88.152:5001';
 const domain = 'http://192.168.1.173:5000';
 const subService = '/pbs-service';
 

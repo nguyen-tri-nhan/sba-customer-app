281c457bb4e1f4ab9f39d0876aad300366592039
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-04-08 00:31:14 +0700

    feedback

--- utils/Constants.js
+++ utils/Constants.js
@@ -11,6 +11,11 @@ export const endPoint = `${domain}${subService}`;
 
 export const STATUS = {
   ENABLE: 'ENABLE',
+  FINISH: 'FINISH',
+  CANCELED: 'CANCELED',
+  PENDING: 'PENDING',
+  PROCESSING: 'PROCESSING',
+  ONBOARD: 'ONBOARD',
 }
 
 export const ENTITY = {
@@ -20,4 +25,12 @@ export const ENTITY = {
   ADDITIONAL_ITEM: 'addition-item',
   STYLE: 'style',
   SLOT: 'slot',
+  FEEDBACK: 'feedback',
+}
+
+export const STATUS_TRANS = {
+  FINISH: 'Hoàn thành',
+  CANCELED: 'Hủy',
+  PENDING: 'Đang chờ',
+  PROCESSING: 'Đang thực hiện',
 }
